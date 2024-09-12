bbcbc4fcc52e959b84a71ac583bb830b4ffaa180
Siddhesh <siddheshsawant7801@gmail.com>
2024-09-12 19:19:11 +0530

    security config

---
+++ book-network/src/main/java/com/BSN/book/user/Token.java
@@ -0,0 +1,32 @@
+package com.BSN.book.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String token;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime expiredAt;
+
+    private LocalDateTime validatedAt;
+
+    @ManyToMany
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+}
